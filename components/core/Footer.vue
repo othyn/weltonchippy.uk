fde7f3e17a408e6879896701ef7d97720c585794
Ben <git@othyn.com>
2023-01-23 21:15:19 +0000

    Version bump

--- components/core/Footer.vue
+++ components/core/Footer.vue
@@ -27,7 +27,7 @@
           <a
             class="font-bold hover:underline"
             href="https://github.com/othyn/weltonchippy.uk/releases"
-            >v1.0.1</a
+            >v1.1.4</a
           >
           <span class="mx-4">|</span>
         </p>
