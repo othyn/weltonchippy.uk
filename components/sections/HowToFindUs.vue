63e380d6b2a3721612f1bd0f77b4ff20a11b9560
Ben <git@othyn.com>
2024-12-17 13:16:41 +0000

    Update Google Maps now that they have the new place

--- components/sections/HowToFindUs.vue
+++ components/sections/HowToFindUs.vue
@@ -1,13 +1,13 @@
 <template>
   <CoreContentBlock title="How To Find Us" secondary>
     <p
-      class="w-full mb-3 text-md font-medium leading-tight text-center text-gray-500"
+      class="w-full mb-3 font-medium leading-tight text-center text-gray-500 text-md"
     >
-      <span class="font-bold">Welton Chippy (Kedgeree)</span>, 8 Cliff Road,
-      Welton, Lincoln, LN2 3JJ
+      <span class="font-bold">Welton Chippy</span>, 8 Cliff Road, Welton,
+      Lincoln, LN2 3JJ
     </p>
     <p
-      class="w-full mb-8 text-md font-medium leading-tight text-center text-gray-500"
+      class="w-full mb-8 font-medium leading-tight text-center text-gray-500 text-md"
     >
       Telephone Number
       <a class="font-bold hover:underline" href="tel:01673860312"
@@ -20,12 +20,14 @@
       scrolling="no"
       marginheight="0"
       marginwidth="0"
-      src="https://maps.google.com/maps?width=100%25&amp;height=400&amp;hl=en&amp;q=Kedgeree,%208%20Cliff%20Road,%20Welton,%20Lincoln+(Kedgeree)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      frameborder="0"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2384.067723922237!2d-0.4877074866971681!3d53.306233377607086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48785ea6e19e770d%3A0x13fedf91aa265a64!2sWelton%20Chippy!5e0!3m2!1sen!2suk!4v1734441278676!5m2!1sen!2suk"
       width="100%"
       height="400"
-      frameborder="0"
-    >
-    </iframe>
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
   </CoreContentBlock>
 </template>
 
