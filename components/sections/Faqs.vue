f8e43eb25cdb64e962180eeaf9133a9d401c788a
Ben <git@othyn.com>
2024-12-17 13:16:51 +0000

    Remove COVID notice

--- components/sections/Faqs.vue
+++ components/sections/Faqs.vue
@@ -3,7 +3,7 @@
     <h4 class="w-full mt-8 text-lg font-bold leading-tight text-gray-800">
       <span class="pr-1">💳</span> Do you take card?
     </h4>
-    <p class="w-full mt-4 text-md font-medium leading-tight text-gray-500">
+    <p class="w-full mt-4 font-medium leading-tight text-gray-500 text-md">
       Yes! We offer both contactless and Chip(s) & Pin. We also offer over the
       phone card payments. You may just have to come and try it for yourself
       sometime, the chips are lovely this time of year.
@@ -12,7 +12,7 @@
     <h4 class="w-full mt-8 text-lg font-bold leading-tight text-gray-800">
       <span class="pr-1">🤔</span> Do you do gluten free?
     </h4>
-    <p class="w-full mt-4 text-md font-medium leading-tight text-gray-500">
+    <p class="w-full mt-4 font-medium leading-tight text-gray-500 text-md">
       Sure do! We please ask that you call ahead, to make sure that we can do
       our best. However, we don't take gluten free orders on Friday nights, due
       to it being the busiest time for us.
@@ -21,18 +21,17 @@
     <h4 class="w-full mt-8 text-lg font-bold leading-tight text-gray-800">
       <span class="pr-1">📞</span> Do you take phone orders?
     </h4>
-    <p class="w-full mt-4 text-md font-medium leading-tight text-gray-500">
-      Give us a bell! However, during these COVID times, phone orders must be
-      paid by card over the phone prior to collection. It is also important to
-      note that we don't take gluten free orders on Friday nights, due to it
-      being the busiest time for us.
+    <p class="w-full mt-4 font-medium leading-tight text-gray-500 text-md">
+      Give us a bell! Phone orders must be paid by card over the phone prior to
+      collection. It is also important to note that we don't take gluten free
+      orders on Friday nights, due to it being the busiest time for us.
     </p>
 
     <h4 class="w-full mt-8 text-lg font-bold leading-tight text-gray-800">
       <span class="pr-1">🎁</span> Do you take orders for events or special
       occasions?
     </h4>
-    <p class="w-full mt-4 text-md font-medium leading-tight text-gray-500">
+    <p class="w-full mt-4 font-medium leading-tight text-gray-500 text-md">
       Absolutely! To ensure that we can do our best to help make your day that
       little bit more special, we ask for a minimum of 48 hours notice.
     </p>
